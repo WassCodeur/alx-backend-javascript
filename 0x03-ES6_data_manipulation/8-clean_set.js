d7d36dd4e2f4e735eb957ee103f888dc19b2c7ed
WassCodeur <wachioubouraima56@gmail.com>
2023-10-15 15:36:53 +0000

    update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,2 +1,10 @@
-export default function cleanSet(set, str) {
+export default function cleanSet(mySet, startStr) {
+  // [...mySet] convert a set to an array
+  const len = startStr.length;
+  let result = '';
+  if (len > 0) {
+    const filterArray = [...mySet].filter((value) => value.startsWith(startStr));
+    result = filterArray.map((value) => value.substring(len)).join('-');
+  }
+  return result;
 }
---
+++ 0x03-ES6_data_manipulation/test.js
@@ -0,0 +1,5 @@
+export default function cleanSet(set, str) {
+}
+console.log(cleanSet([12, 32, 15, 78, 98, 15]));
+
+
