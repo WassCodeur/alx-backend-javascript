c15a6728b3522d9917efb9baa0cf2244f44724e9
WassCodeur <wachioubouraima56@gmail.com>
2023-10-15 19:07:18 +0000

    Revert "update 8-clean_set.js"
    This reverts commit f047a686635f6fd846580d6779e95dd1d5317b5c.

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(mySet, startStr) {
   // [...mySet] convert a set to an array
-  const len = startStr.length;
-  if (!mySet || !startStr || !(mySet instanceof Set) || typeof startStr !== 'string' || len <= 0) {
+  if (!mySet || !startStr || !(mySet instanceof Set) || typeof startStr !== 'string') {
     return '';
   }
+  const len = startStr.length;
   let result = [];
   if (len > 0) {
     const filterArray = [...mySet].filter((value) => value.startsWith(startStr));
