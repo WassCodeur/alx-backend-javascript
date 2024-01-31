72f50de04671b2b9b186e098f67b7e75a4a139c3
WassCodeur <wachioubouraima56@gmail.com>
2024-01-31 16:33:16 +0000

    Fix formatting and update input/output handling

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,10 @@
-process.stdin.setEncoding("utf-8");
+process.stdin.setEncoding('utf-8');
 
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-process.stdin.on("readable", function() {
-	const data = process.stdin.read();
-	if(data != null) {
-		process.stdout.write("Your name is: " + data);
-	}
-})
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+});
 
-process.stdin.on('end', function() { 
-	process.stdout.write("This important software is now closing");
-})
+process.stdin.on('close', () => {
+  process.stdout.write('This important software is now closing');
+});
