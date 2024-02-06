e2293318733677061752c8728598a9f85b0de94d
WassCodeur <wachioubouraima56@gmail.com>
2024-02-06 21:58:40 +0000

    update the print function

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdin.setEncoding('utf-8');
 
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
 });
