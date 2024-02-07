3e9a61321cd1d1cf9eeeecc290966964cb6c2905
WassCodeur <wachioubouraima56@gmail.com>
2024-02-07 01:41:48 +0000

    Refactor student counting logic in /students route

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -5,44 +5,47 @@ const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   if (req.url === '/') {
     res.write('Hello Holberton School!');
+    res.end();
   } else if (req.url === '/students') {
-    if (process.argv.length === 3) {
+    if (process.argv.length >= 3) {
       const path = process.argv[2];
-      const csArr = [];
-      const sweArr = [];
-      let stdsArr = [];
-      let csNb = 0;
-      let sweNb = 0;
       fs.readFile(path, 'utf8', (err, data) => {
         if (err) {
           throw new Error('Cannot load the database');
         } else {
-          const dataArr = data.split('\n');
-          dataArr.pop();
-          const len = dataArr.length;
-          for (let i = 1; i < len;) {
-            stdsArr = dataArr[i].split(',');
-            if (stdsArr[3] === 'CS') {
-              csNb += 1;
-              csArr.push(stdsArr[0]);
-            } else if (stdsArr[3] === 'SWE') {
-              sweNb += 1;
-              sweArr.push(stdsArr[0]);
+          const lines = data.split(/\r?\n/);
+          const FIELDS = {};
+          let nbOfStudents = lines.length - 1;
+          for (let i = 1; i < lines.length;) {
+            if (lines[i].trim() !== '') {
+              const [fname, lname, age, field] = lines[i].split(','); // eslint-disable-line
+              if (!FIELDS[field]) {
+                FIELDS[field] = {
+                  count: 1,
+                  students: [fname],
+                };
+              } else {
+                const nwCount = FIELDS[field].count + 1;
+                const nwStudents = FIELDS[field].students.concat(fname);
+                FIELDS[field] = {
+                  count: nwCount,
+                  students: nwStudents,
+                };
+              }
+            } else {
+              nbOfStudents -= 1;
             }
             i += 1;
           }
 
           res.write('This is the list of our students\n');
-          res.write(`Number of students: ${len - 1}\n`);
-          if (csNb >= sweNb) {
-            res.write(`Number of students in CS: ${csNb}. List: ${csArr.join(', ')}\n`);
-            res.write(`Number of students in CS: ${sweNb}. List: ${sweArr.join(', ')}`);
-            res.end();
-          } else {
-            res.write(`Number of students in CS: ${sweNb}. List: ${sweArr.join(', ')}\n`);
-            res.write(`Number of students in CS: ${csNb}. List: ${csArr.join(', ')}`);
-            res.end();
+          res.write(`Number of students: ${nbOfStudents}\n`);
+          for (const field of Object.keys(FIELDS)) {
+            const { count } = FIELDS[field];
+            const { students } = FIELDS[field];
+            res.write(`Number of students in field: ${count}. List: ${students.join(', ')}\n`);
           }
+          res.end();
         }
       });
     }
